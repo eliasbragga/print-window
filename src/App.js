c1ffff1429e298e8be14eb1417ff49c3f488a0d5
Elias notebook <eliasbragga44@gmail.com>
2023-01-29 23:34:48 -0300

    feat:adjusting functions

--- src/App.js
+++ src/App.js
@@ -1,49 +1,97 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { marginBottom } from "responsively/dist/utils/marginFunctions/marginFunctions";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function App() {
-
-  const [dot, setDot] = useState([])
+  const [dot, setDot] = useState([]);
+  const [reUndo, setreUndo] = useState([]);
+  const [undo, setUndo] = useState([]);
+  const [lastItem, setLastItem] = useState([]);
 
   const undoDot = (event) => {
-    event.stopPropagation()
-  }
+    event.stopPropagation();
+    if (dot.length > 0) {
+      let lastItemAUX = dot.splice(dot.length - 1, 1);
+      setreUndo(() => [...reUndo, ...lastItemAUX]);
+      let teste = dot.filter(
+        (e) =>
+          e.clientX !== lastItemAUX[0].clientX && e.clientY !== lastItemAUX[0].clientY
+      )
+      setDot(teste);
+    }
+  };
 
   const reundoDot = (event) => {
-    event.stopPropagation()
-  }
-
+    event.stopPropagation();
+    if (reUndo.length > 0) {
+      let lastItemAUX = reUndo.splice(reUndo.length - 1, 1);
+      setDot(() => [...dot, ...lastItemAUX]);
+    }
+  };
 
   const clearDots = (event) => {
-    event.stopPropagation()
-    setDot([])
-  }
+    event.stopPropagation();
+    if (dot.length > 0) {
+      setDot([]);
+    }
+  };
 
   const handleClick = (event) => {
- 
     const position_x = {
       clientX: event.clientX,
-      clientY: event.clientY
-    }
+      clientY: event.clientY,
+    };
+    setDot((event) => [...event, position_x]);
+  };
 
-    setDot((event) => [...event,position_x])
-    console.log(dot)
-  }
+  
+  const handleKeyDown = (event) => {
+    if (event.ctrlKey && event.key === "z") {
+      if (dot.length > 0) {
+        let lastItemAUX = dot.splice(dot.length - 1, 1);
+        setreUndo(() => [...reUndo, ...lastItemAUX]);
+        let teste = dot.filter(
+          (e) =>
+            e.clientX !== lastItemAUX.clientX && e.clientY !== lastItemAUX.clientY
+        );
+        setDot(teste);
+      }
+    }
+  };
+
+    useEffect(() => {
+      document.addEventListener("keydown", handleKeyDown);
+      return () => {
+        document.removeEventListener("keydown", handleKeyDown);
+      };
+    }, []);
+ 
 
   return (
     <div className="App">
+      <div className="buttons">
+        <button onClick={undoDot} style={{ marginRight: "5px" }}>
+          Desfazer
+        </button>
+        <button onClick={reundoDot} style={{ marginLeft: "5px" }}>
+          Refazer
+        </button>
+        <button onClick={clearDots} style={{ marginLeft: "5px" }}>
+          Limpar
+        </button>
+        {dot.map((e) => {
+          return (
+            <span
+            onDoubleClick={undoDot}
+              className="star"
+              style={{ top: e.clientY, left: e.clientX }}
+            ></span>
+          );
+        })}
+
+      </div>
       <div className="container" onClick={handleClick}>
-        <button onClick={undoDot} style={{ marginRight: "5px" }}>Desfazer</button>
-        <button onClick={reundoDot} style={{ marginLeft: "5px" }}>Refazer</button>
-        <button onClick={clearDots} style={{ marginLeft: "5px" }}>Limpar</button>
-        {
-          dot.map((e) => {
-           return <span  className="dot" style={{top:e.clientY, left:e.clientX}}></span>
-          })
-        }
-        
       </div>
     </div>
   );
